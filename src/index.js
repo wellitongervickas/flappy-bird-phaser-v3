cc2a14ff7cea07778f5ace6e6e0b46df20bacd00
welliton gervickas <wellitogervickas@gmail.com>
2023-02-28 14:45:59 -0300

    feat: init project

--- src/index.js
+++ src/index.js
@@ -1,28 +1,52 @@
-
 import Phaser from "phaser";
 
 const config = {
+  // WebGL (Web graphics library) API form rendering 2 and 3D graphics
   type: Phaser.AUTO,
   width: 800,
   height: 600,
   physics: {
-    default: 'arcade',
-    arcade: {
-      gravity: { y: 200 }
-    }
+    // Arcade physics plugin, anages physics simulation
+    default: "arcade",
   },
   scene: {
-    preload: preload,
-    create: create
-  }
+    preload,
+    update,
+    create,
+  },
 };
 
 new Phaser.Game(config);
 
-function preload () {
-  this.load.image('sky', 'assets/sky.png');
+// loading assets, such as image, music, animations
+function preload() {
+  // 'this' context = scene
+  // contains functions and properties we can use
+  this.load.image("sky", "assets/sky.png");
 }
 
-function create () {
-  this.add.image(400, 300, 'sky');
+// create instance objects, interactions, basic setup
+function create() {
+  // this.add.image(config.width / 2, config.height / 2, "sky");
+  // or change the origin
+
+  // origin canvas middle= x, y
+  this.add.image(0, 0, "sky").setOrigin(0, 0); // origin of image middle y , x
+  /**
+   * 
+   * x=0.0-------x=0.5-------x=1.0
+   * y=0.0-------y=0.0-------y=0.0
+   * 
+   * 
+   * x=0.0-------x=0.5-------x=1.0
+   * y=0.5-------y=0.5-------y=0.5
+   * 
+   * 
+   * x=0.0-------x=0.5-------x=1.0
+   * y=1.0-------y=1.0-------y=1.0
+
+   */
 }
+
+// animaton loop
+function update() {}
