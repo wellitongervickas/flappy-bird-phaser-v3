59255bc59795fb7a3fdc71b41ba391bdde9558bb
welliton gervickas <wellitogervickas@gmail.com>
2023-03-02 14:26:55 -0300

    refacoty to group pipes

--- src/index.js
+++ src/index.js
@@ -29,10 +29,10 @@ const config = {
 
 new Phaser.Game(config);
 
-let bird, upperPipe, lowerPipe;
+let bird, pipes;
 
-const pipeVeticalDistanceRange = [150, 200];
-let pipeHorizontalDistance = 0;
+const PIPE_VERTICAL_DISTANCE_RANGE = [150, 200];
+const PIPE_HORIZONTAL_DISTANCE_RANGE = [450, 550];
 
 const PIPES_TO_RENDER = 4;
 const FLAP_VELOCITY = 300;
@@ -78,33 +78,52 @@ function create() {
 
   bird.body.gravity.y = 600;
 
+  pipes = this.physics.add.group();
+
   for (let i = 0; i < PIPES_TO_RENDER; i++) {
-    pipeHorizontalDistance += 400;
+    const upperPipe = pipes.create(0, 0, "pipe").setOrigin(0, 1);
+    const lowerPipe = pipes.create(0, 0, "pipe").setOrigin(0, 0);
 
-    const pipeVerticalDistance = Phaser.Math.Between(
-      ...pipeVeticalDistanceRange
-    );
-
-    const pipeVerticalPosition = Phaser.Math.Between(
-      20,
-      config.height - 20 - pipeVerticalDistance
-    );
-
-    upperPipe = this.physics.add
-      .sprite(pipeHorizontalDistance, pipeVerticalPosition, "pipe")
-      .setOrigin(0, 1);
-
-    lowerPipe = this.physics.add
-      .sprite(upperPipe.x, upperPipe.y + pipeVerticalDistance, "pipe")
-      .setOrigin(0, 0);
-
-    upperPipe.body.velocity.x = -200;
-
-    lowerPipe.body.velocity.x = -200;
+    placePipe(upperPipe, lowerPipe);
   }
+
+  pipes.setVelocityX(-200);
 
   this.input.on("pointerdown", flat);
   this.input.keyboard.on("keydown-SPACE", flat);
+}
+
+function placePipe(upperPipe, lowerPipe) {
+  const rightMostX = getRightMostPipe();
+
+  const pipeVerticalDistance = Phaser.Math.Between(
+    ...PIPE_VERTICAL_DISTANCE_RANGE
+  );
+
+  const pipeHorizontalDistance = Phaser.Math.Between(
+    ...PIPE_HORIZONTAL_DISTANCE_RANGE
+  );
+
+  const pipeVerticalPosition = Phaser.Math.Between(
+    20,
+    config.height - 20 - pipeVerticalDistance
+  );
+
+  upperPipe.x = rightMostX + pipeHorizontalDistance;
+  upperPipe.y = pipeVerticalPosition;
+
+  lowerPipe.x = upperPipe.x;
+  lowerPipe.y = upperPipe.y + pipeVerticalDistance;
+}
+
+function getRightMostPipe() {
+  let rightMostX = 0;
+
+  pipes.getChildren().forEach(function (pipe) {
+    rightMostX = Math.max(pipe.x, rightMostX);
+  });
+
+  return rightMostX;
 }
 
 // animaton loop
